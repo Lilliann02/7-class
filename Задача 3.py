f01d6a5a4042ec2f6fd2b1c2f0e0ea19917d14e1
Lilliann02 <lilliann.shv@gmail.com>
2024-11-19 23:57:42 +0200

    3+4

---
+++ Задача 3.py
@@ -0,0 +1,8 @@
+x = float(input("x="))
+y = float(input("y="))
+if x==y:
+    print("числа рівні")
+elif x<y:
+    print("Друге число більше")
+else:
+    print("Перше число більше")
---
+++ Задача 4.py
@@ -0,0 +1,11 @@
+x1 = float(input("Перше число = "))
+x2 = float(input("Друге число = "))
+x3 = float(input("Третє число = "))
+x4 = float(input("Четверте число = "))
+if x1>x2: max = x1
+else: max = x2
+if x3 > max: max = x3
+if x4 > max: max = x4
+print ("Найбільше число", max)
+
+    
